0711014fd551de1e2e81fa032793e2a575e25ec8
Pratim <bboyfang97@gmail.com>
2026-03-13 07:10:33 +0530

    fix cors

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ import OpenAI from "openai";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 const client = new OpenAI({
